e9f15ae95af0ac2e58a62e1e9ee7c5753343d260
Atul Pandey <atulpandeyy25@gmail.com>
2026-03-20 01:37:20 +0530

    Redifined drawable words

--- scribble/src/utilities/scribbleWord.js
+++ scribble/src/utilities/scribbleWord.js
@@ -1,21 +1,3 @@
-// export const getScribbleWord = (roomId,word = "", revealedHintIndexes = []) => {
-//     const revealedHints = new Set(revealedHintIndexes)
-//     return word
-//         .split("")
-//         .map((char, index) => {
-//             if (char === " ") return " "
-//             if (revealedHints.has(index)) {
-//                 return char
-//             }
-
-//             return "_"
-//         })
-//         .join(" ")
-
-
-// }
-
-
 
 
 export const scoreDisplay=(drawerId,room,guessed)=>{
